7c3882522403a4e1f4dba927b77666c3ef2a6df1
Chanyoon Zhu <zhuchanyoon@gmail.com>
2019-02-23 14:42:32 -0500

    adds writing to files functionality

--- tcp/client.c
+++ tcp/client.c
@@ -18,7 +18,7 @@
 #define BUFLEN 1024 
 #define FILENAMELEN 30
 
-int readFilenames(char* file, char** filenames);
+int readFilenames(char* file, char*** filenames);
 
 int main(int argc, char **argv) { 
     
@@ -29,6 +29,8 @@ int main(int argc, char **argv) {
     socklen_t slen = sizeof(serverAddr);
     char** filenames;
     int filecount;
+    int recv_len;
+
     /*socket creation*/
     if ((serverSock = socket(PF_INET, SOCK_STREAM, 0)) < 0) { 
         perror("socket failed"); 
@@ -54,18 +56,40 @@ int main(int argc, char **argv) {
     }
 
     // read file
-    if ((filecount = readFilenames(argv[1], filenames)) < 0) {
+    if ((filecount = readFilenames(argv[1],&filenames)) < 0) {
         perror("can't read file.\n");
         exit(1);
     }
 
     for (int i = 0; i < filecount; i++) {
         // send filenames
-        if (send(serverSock, "Hi", FILENAMELEN, 0) != FILENAMELEN) {
+        if (send(serverSock, *(filenames+i), FILENAMELEN, 0) != FILENAMELEN) {
             perror("filename sent error\n");
         } else {
             // server replys 1. success 0. failure
-            printf("success\n");  
+            memset(buffer, '\0', BUFLEN);
+            if (recv_len = recv(serverSock, buffer, BUFLEN, 0) < 0) {
+                perror("success/fail status not received\n");
+                exit(1);
+            }
+            if (strcmp(buffer, "Success\0") == 0) {
+                printf("Success\n");
+                while (strcmp(buffer, "DONE!\n") != 0) {
+                    memset(buffer, '\0', BUFLEN);
+                    fgets(buffer, BUFLEN, stdin);
+                    if (send(serverSock, buffer, BUFLEN, 0) != BUFLEN) {
+                        perror("data sent error\n");
+                    }
+                }
+                memset(buffer, '\0', BUFLEN);
+                if (recv_len = recv(serverSock, buffer, BUFLEN, 0) < 0) {
+                    perror("end file acknowledge not received\n");
+                } else {
+                    printf("server closed file\n");
+                }
+            } else {
+                printf("timeout, failed sending filename");
+            }
         }
     }
 
@@ -73,34 +97,12 @@ int main(int argc, char **argv) {
     // recv server ack
     // acknowledge ack
 
-    /*
-    // open pcap file
-    pp = pcap_open_offline(argv[1], errbuf);
-    if (pp == NULL) {
-        fprintf(stderr, "\npcap_open_offline() failed: %s\n", errbuf);
-        return 0;
-    }
-    
-    
-    while ((packet = pcap_next(pp, &header)) != NULL) {
-        
-        int i = 0;
-
-        if (sendto(serverSock, packet, header.len, 
-            MSG_CONFIRM, (const struct sockaddr *) &serverAddr,  
-            slen) < 0) {
-            perror("send error\n");
-        } else {
-            printf("packet sent\n");
-        } 
-    }
-    */
     close(serverSock); 
     return 0;
 }
 
 
-int readFilenames(char* file, char** filenames)
+int readFilenames(char* file, char*** filenames)
 {
     FILE *fp;
     int count = 0;
@@ -119,12 +121,12 @@ int readFilenames(char* file, char** filenames)
     }
     // point to file beginning 
     fseek(fp, 0, SEEK_SET);
-    filenames = malloc(sizeof(char*) * count);      
+    *filenames = (char **)malloc(sizeof(char*) * count);      
     for (int i = 0; i < count; i++) {
         memset(buffer, 0, sizeof(buffer));
         fscanf(fp, "%s\n", buffer);
-        filenames[i] = malloc(sizeof(buffer));
-        strcpy(filenames[i], buffer);
+        *(*filenames+i) = (char *)malloc(sizeof(char) * FILENAMELEN);
+        strcpy(((*filenames)[i]), buffer);
     }
 
     return count; 
--- tcp/server.c
+++ tcp/server.c
@@ -17,13 +17,16 @@
  
 #define BUFLEN 1024
 #define PORT 8080
+#define FILENAMELEN 30
+
 int main(void)
 {
     char buffer[BUFLEN];
-    //char packet[BUFLEN];
+    char filename[FILENAMELEN];
     struct sockaddr_in serverAddr, clientAddr;
     int serverSock;
     int fd;
+    FILE *fptr;
     int recv_len;
     int queue_limit = 10;
     socklen_t slen = sizeof(clientAddr); 
@@ -51,21 +54,48 @@ int main(void)
         perror("listening failed\n");
     }
 
+    // socket accept
+    if ((fd = accept(serverSock, (struct sockaddr*)&clientAddr, &slen)) < 0){
+        perror("accept failed\n");
+    }
     
     while(1)
     {
-        // socket accept
-        if ((fd = accept(serverSock, (struct sockaddr*)&clientAddr, &slen)) < 0){
-            perror("accept failed\n");
-        }
-        /*receive packet*/
         memset(buffer,'\0', BUFLEN);
+        // TODO: timeout
         if ((recv_len = recv(fd, buffer, BUFLEN, 0)) < 0) {
             perror("receiving failed\n");
         } else {
-            //parsePacket(buffer, recv_len);
-            printf("%s\n", buffer);
-        }
+            if(send(fd, "Success\0", 8, 0) != 8) {
+                perror("success/fail acknowledge failed\n");
+                exit(1);
+            }
+            printf("writing file: %s\n", buffer);
+            if ((fptr = fopen(buffer, "w+")) == NULL) {
+                perror("can't create file\n");
+                exit(1);
+            }
+
+            memset(buffer, '\0', BUFLEN);
+            if ((recv_len = recv(fd, buffer, BUFLEN, 0)) < 0) {
+                perror("receiving data failed\n");
+            }
+            while (strcmp(buffer, "DONE!\n") != 0) {
+                printf("writing data %s\n", buffer);
+                fputs(buffer, fptr);
+                memset(buffer, '\0', BUFLEN);
+                if ((recv_len = recv(fd, buffer, BUFLEN, 0)) < 0) {
+                    perror("receiving data failed\n");
+                }
+            }
+            if ((send(fd, "THANK YOU! FILE CLOSED\0", BUFLEN, 0)) != BUFLEN) {
+                perror("file close acknowledge not sent\n");
+            } else {
+                printf("file close ack sent\n");
+            } 
+            fclose(fptr);
+            printf("done writing file\n");
+       }
     }
  
     close(serverSock);
